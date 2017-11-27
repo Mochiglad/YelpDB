9f443861ffe94a7da68c30a60fedf526f89f89f5
Mochiglad <stevenseraser@gmail.com>
2017-11-26 16:58:15 -0800

    Yelp stuff

---
+++ src/main/java/ca/ece/ubc/cpen221/mp5/YelpRestaurant.java
@@ -0,0 +1,15 @@
+package ca.ece.ubc.cpen221.mp5;
+
+import java.util.HashSet;
+
+public class YelpRestaurant extends GenericRestaurant{
+
+	public YelpRestaurant(String id, boolean status, String url, double longitude, double latitude,
+			HashSet<String> neighborhoods, HashSet<String> categories, HashSet<String> schools, String businessId,
+			String name, String state, String BType, int stars, String city, String fullAddress, int reviewCount,
+			String photoUrl, int price) {
+		super(id, status, url, longitude, latitude, neighborhoods, categories, schools, businessId, name, state, BType, stars,
+				city, fullAddress, reviewCount, photoUrl, price);
+	}
+	
+}
